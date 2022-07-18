09798d931bcdc252361f843ad74258cd2f878273
ehsunoo <info.hamrah@gmail.com>
2022-07-18 17:17:49 +0430

    Add API

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://webhook.site/2a6f2fb0-76fc-4eb5-bd4d-ec0ed739e793");
-  // const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=da0d44b54759bdcd2d6240fe1de2f2d9&language=en-US&page=1");
+  const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=da0d44b54759bdcd2d6240fe1de2f2d9&language=en-US&page=1");
   const jsonData = await response.json();
   res.status(200).json(jsonData);
 }
