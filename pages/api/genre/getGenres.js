f65d4319e67e9e2f217090678925e6dbeb0429a6
ehsunoo <info.hamrah@gmail.com>
2022-07-24 00:08:25 +0430

    Update getGenre.js & [id].js

---
+++ pages/api/genre/getGenres.js
@@ -0,0 +1,5 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://next-teal-eta.vercel.app/api/getGenres");
+  const data = await response.json();
+  res.status(200).json(data);
+}
---
+++ pages/genres/[genre].js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Content from "../../components/Content";
+import { useDispatch, useSelector } from "react-redux";
+
+function Genres() {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => console.log(state));
+
+  // router.query.genre
+
+  return <Content title={router.query.genre} movies={movies} />;
+}
+
+export default Genres;
