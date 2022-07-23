4b7065acf1a5440d9fdfe3853a252e9b80c6759d
ehsunoo <info.hamrah@gmail.com>
2022-07-23 21:24:05 +0430

    Add getGenre.js

--- pages/api/getGenre.js
+++ pages/api/getGenre.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=da0d44b54759bdcd2d6240fe1de2f2d9&language=en-US");
+  const { genre } = req.query;
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=da0d44b54759bdcd2d6240fe1de2f2d9&with_genres=${genre}`);
   const data = await response.json();
   res.status(200).json(data);
 }
