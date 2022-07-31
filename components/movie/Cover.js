7412764782640147f67b97fe85d4c475204e7fe6
Ehsan Nouri <info.hamrah@gmail.com>
2022-07-31 23:04:19 +0430

    Replace img by Image tag.

--- components/movie/Cover.js
+++ components/movie/Cover.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "next/image";
 
 import styles from "./styles/Cover.module.scss";
 
 function Cover({ movie }) {
   return (
     <div className={styles.cover}>
-      <img src={`https://image.tmdb.org/t/p/w780/${movie?.poster_path}`} decoding="async" />
+      <Image src={`https://image.tmdb.org/t/p/w780/${movie?.poster_path}`} width={330} height={495} layout="responsive" />
     </div>
   );
 }
