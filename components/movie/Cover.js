efd81307af3a7ff1b84c85fae3b73e1c1cf32521
Ehsan Nouri <info.hamrah@gmail.com>
2022-08-01 00:00:12 +0430

    Update Image tag

--- components/movie/Cover.js
+++ components/movie/Cover.js
@@ -6,7 +6,7 @@ import styles from "./styles/Cover.module.scss";
 function Cover({ movie }) {
   return (
     <div className={styles.cover}>
-      <Image src={`https://image.tmdb.org/t/p/w780${movie?.poster_path}`} width={330} height={495} layout="responsive" />
+      <Image src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`} width={330} height={495} layout="responsive" />
     </div>
   );
 }
