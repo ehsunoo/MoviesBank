a6f903daaf144da52c1d61758978abffa47c0865
Ehsan Nouri <info.hamrah@gmail.com>
2022-08-01 12:34:17 +0430

    Update cast image condition

--- components/movie/Content.js
+++ components/movie/Content.js
@@ -44,7 +44,7 @@ function Content({ movie, cast }) {
             <li key={item?.id}>
               <Link href={`/person/${item?.id}`}>
                 <a>
-                  {item?.profile_path !=="null" ? (
+                  {item.profile_path !== "null" ? (
                     <Image src={`https://image.tmdb.org/t/p/w185${item?.profile_path}`} width={24} height={24} title={item?.name} alt={item?.name} objectFit="cover" layout="responsive" />
                   ) : (
                     <Image src={`/assets/person.svg`} width={24} height={24} title={item?.name} alt={item?.name} objectFit="cover" layout="responsive" />
