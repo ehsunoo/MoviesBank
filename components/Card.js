0bdbbdc6df38e596a2d81332c174b8db6a3fd938
Ehsan Nouri <info.hamrah@gmail.com>
2022-08-01 12:47:06 +0430

    Remove additional hooks

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./styles/Card.module.scss";
--- components/movie/Cover.js
+++ components/movie/Cover.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 
 import { useImage } from "../../hooks/useImage";
