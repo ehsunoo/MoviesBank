199758dd8e63e15dfd018e4f6fcd40a87ae2980f
Ehsan Nouri <info.hamrah@gmail.com>
2022-08-01 17:24:20 +0430

    Implement Loading component

--- components/Card.js
+++ components/Card.js
@@ -23,7 +23,7 @@ function Card({ movie }) {
     <div className={styles.container}>
       <Link href={`/movie/${movie?.id}`}>
         <a>
-          <div className={styles.cover}>{loaded ? <Image src={coverSrc} width={230} height={345} layout="responsive" alt={movie?.title} objectFit="contain" onError={coverErrorHandler} onLoadingComplete={onLoadHandler} /> : <Loading width={10} height={10} />}</div>
+          <div className={styles.cover}>{loaded ? <Image src={coverSrc} width={230} height={345} layout="responsive" alt={movie?.title} objectFit="contain" onError={coverErrorHandler} onLoad={onLoadHandler} /> : <Loading width={10} height={10} />}</div>
           <div className={styles.details}>
             <p>{movie?.title}</p>
           </div>
