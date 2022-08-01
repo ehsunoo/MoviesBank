4d05f247b734c54a8a38365e4ffb2565018f2995
Ehsan Nouri <info.hamrah@gmail.com>
2022-08-01 13:47:10 +0430

    Update useImage hook

--- hooks/useImage.js
+++ hooks/useImage.js
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useImage = (inputImageSrc) => {
   const [imageSrc, setImageSrc] = useState(inputImageSrc);
 
-  function imageSrcErrorHandler() {
+  useEffect(() => {
+    setImageSrc(inputImageSrc);
+  }, [inputImageSrc]);
+
+  const imageSrcErrorHandler = useCallback(() => {
     setImageSrc(`${process.env.BASE_URL}/assets/noimage.png`);
-  }
+  }, []);
 
   return [imageSrc, imageSrcErrorHandler];
 };
