479cd5d0013cab478e888f330539cbf9de6da03c
Ehsan Nouri <info.hamrah@gmail.com>
2022-08-01 16:28:55 +0430

    Create Loading component and add to Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./styles/Card.module.scss";
 import { useImage } from "../hooks/useImage";
+import Loading from "./Loading";
 
 function Card({ movie }) {
   const imgSrc = `${process.env.IMAGE_BASE_URL}/w300${movie?.poster_path}`;
   const [coverSrc, coverErrorHandler] = useImage(imgSrc);
+
+  const [loaded, setLoaded] = useState(true);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  const onLoadHandler = useCallback(() => {
+    setLoaded(false);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Link href={`/movie/${movie?.id}`}>
         <a>
-          <div className={styles.cover}>
-            <Image src={coverSrc} width={230} height={345} layout="responsive" alt={movie?.title} objectFit="contain" onError={coverErrorHandler} />
-          </div>
+          <div className={styles.cover}>{!loaded ? <Image src={coverSrc} width={230} height={345} layout="responsive" alt={movie?.title} objectFit="contain" onError={coverErrorHandler} onLoad={onLoadHandler} /> : <Loading width={30} height={30} />}</div>
           <div className={styles.details}>
             <p>{movie?.title}</p>
           </div>
